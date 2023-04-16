34038d7bfcad992636c5c4bf50619986f31350b8
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-16 05:44:36 +0200

    Refactor prototype.ts and add tests

---
+++ app/prototype.test.ts
@@ -0,0 +1,71 @@
+import {
+  getUsage,
+  isEconomy,
+  isPremium,
+  pickEconomyGuests,
+  pickPremiumGuests,
+  sortNumbersDescending
+} from './prototype';
+
+const guests = [23, 45, 155, 374, 22, 99, 100, 101, 115, 209];
+describe('Prototype', () => {
+  it('should be an economy', () => {
+    const guest = 56;
+
+    expect(isEconomy(guest)).toBe(true);
+  });
+
+  it('should not be an economy guest', () => {
+    const guest = 123;
+
+    expect(isEconomy(guest)).toBe(false);
+  });
+
+  it('should be a premium guest', () => {
+    const guest = 123;
+
+    expect(isPremium(guest)).toBe(true);
+  });
+
+  it('should not be a premium guest', () => {
+    const guest = 56;
+
+    expect(isPremium(guest)).toBe(false);
+  });
+
+  it('should be pick economy quests', () => {
+    const guests = [23, 45, 155, 374, 22, 99, 100, 101, 115, 209];
+    const economyGuests = [23, 45, 22, 99];
+
+    expect(pickEconomyGuests(guests)).toEqual(economyGuests);
+  });
+
+  it('should be pick premium quests', () => {
+    const guests = [23, 45, 155, 374, 22, 99, 100, 101, 115, 209];
+    const premiumGuests = [155, 374, 100, 101, 115, 209];
+
+    expect(pickPremiumGuests(guests)).toEqual(premiumGuests);
+  });
+
+  it('should sort numbers descending', () => {
+    const guests = [23, 45, 155, 374, 22, 99, 100, 101, 115, 209];
+    const sortedGuests = [374, 209, 155, 115, 101, 100, 99, 45, 23, 22];
+
+    expect(guests.sort(sortNumbersDescending)).toEqual(sortedGuests);
+  });
+
+  it('should get usage for the economy guests', () => {
+    const guests = [23, 45, 155, 374, 22, 99, 100, 101, 115, 209];
+    const economyGuests = pickEconomyGuests(guests);
+    const expectedUsage = 167;
+
+    expect(getUsage(economyGuests, 3)).toBe(expectedUsage);
+  });
+
+  it('should get usage for the premium guests', () => {
+    const premiumGuests = pickPremiumGuests(guests);
+    const expectedUsage = 738;
+
+    expect(getUsage(premiumGuests, 3)).toBe(expectedUsage);
+  });
+});
--- app/prototype.ts
+++ app/prototype.ts
@@ -1,45 +1,15 @@
-const guests = [
-  23,
-  45,
-  155,
-  374,
-  22,
-  99,
-  100,
-  101,
-  115,
-  209
-];
-const isEconomy = (guest: number): boolean => guest < 100;
-const isPremium = (guest: number): boolean => guest >= 100;
+export const isEconomy = (guest: number): boolean => guest < 100;
+export const isPremium = (guest: number): boolean => guest >= 100;
 
-const pickEconomyGuests = (guests: number[]): number[] => guests.filter(isEconomy);
+export const pickEconomyGuests = (guests: number[]): number[] => guests.filter(isEconomy);
 
-const pickPremiumGuests = (guests: number[]): number[] => guests.filter(isPremium);
+export const pickPremiumGuests = (guests: number[]): number[] => guests.filter(isPremium);
 
-const sortNumbersDescending = (a: number, b: number) => b - a;
+export const sortNumbersDescending = (a: number, b: number) => b - a;
 
-const getUsage = (guests: number[], rooms: number): number => {
+export const getUsage = (guests: number[], rooms: number): number => {
   return guests
     .sort(sortNumbersDescending)
     .slice(0, rooms)
     .reduce((usage, guest) => usage + guest);
 };
-
-console.log('Test :: Premium', {
-  premium: {
-    rooms: 3,
-    pickedRooms: pickPremiumGuests(guests),
-    expectedUsage: 738,
-    actualUsage: getUsage(pickPremiumGuests(guests), 3)
-  }
-});
-
-console.log('Test :: Economy', {
-  economy: {
-    rooms: 3,
-    pickedRooms: pickEconomyGuests(guests),
-    expectedUsage: 167,
-    actualUsage: getUsage(pickEconomyGuests(guests), 3)
-  }
-});
