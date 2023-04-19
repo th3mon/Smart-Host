73a987463603e437a7d9dd41248c05f9c3306bf7
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-19 23:40:04 +0200

    Add Test 3

--- app/components/room-occupancy-optimization/room-occupancy-optimization.test.tsx
+++ app/components/room-occupancy-optimization/room-occupancy-optimization.test.tsx
@@ -22,7 +22,7 @@ describe('Room Occupancy Optimization', () => {
     expect(economyUsage).toBeInTheDocument();
   });
 
-  it('should have 738 EUR premium usage and 167 EUR economy usage', async () => {
+  it('[TEST 1] should have 738 EUR premium usage and 167 EUR economy usage', async () => {
     const premiumRooms = 3;
     const economyRooms = 3;
     render(<RoomOccupancyOptimization />);
@@ -45,7 +45,7 @@ describe('Room Occupancy Optimization', () => {
     expect(economyUsage).toHaveTextContent('167 EUR');
   });
 
-  it('should have 1054 EUR premium usage and 189 EUR economy usage', async () => {
+  it('[TEST 2] should have 1054 EUR premium usage and 189 EUR economy usage', async () => {
     const premiumRooms = 7;
     const economyRooms = 5;
     render(<RoomOccupancyOptimization />);
@@ -68,7 +68,30 @@ describe('Room Occupancy Optimization', () => {
     expect(economyUsage).toHaveTextContent('189 EUR');
   });
 
-  it('should have 1153 EUR premium usage and 45 EUR economy usage', async () => {
+  it('[TEST 3] should have 583 EUR premium usage and 189 EUR economy usage', async () => {
+    const premiumRooms = 2;
+    const economyRooms = 7;
+    render(<RoomOccupancyOptimization />);
+    const premiumUsage = screen.getByTestId('premium-usage__value');
+    const economyUsage = screen.getByTestId('economy-usage__value');
+
+    await userEvent.type(
+      screen.getByLabelText(/Premium Rooms/i),
+      String(premiumRooms)
+    );
+
+    await userEvent.type(
+      screen.getByLabelText(/Economy Rooms/i),
+      String(economyRooms)
+    );
+
+    await userEvent.click(screen.getByText(/calculate usage/i));
+
+    expect(premiumUsage).toHaveTextContent('583 EUR');
+    expect(economyUsage).toHaveTextContent('189 EUR');
+  });
+
+  it('[TEST 4] should have 1153 EUR premium usage and 45 EUR economy usage', async () => {
     const premiumRooms = 7;
     const economyRooms = 1;
     render(<RoomOccupancyOptimization />);
