248a7b62b8cbe4d19fe74440d0e80fc751af317f
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-19 04:23:04 +0200

    Setup Jest for testing React code

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,10 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   verbose: true,
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': 'babel-jest',
   },
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
 export default config;
