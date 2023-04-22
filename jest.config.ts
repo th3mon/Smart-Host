7bbb81c518e6ab77c22c801d58ecfaa48f7fd462
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-23 00:31:03 +0200

    Fix deploy issue

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,17 @@
 import type { Config } from '@jest/types';
-// Sync object
-const config: Config.InitialOptions = {
-  verbose: true,
-  transform: {
-    '^.+\\.tsx?$': 'babel-jest',
-  },
-  testEnvironment: 'jsdom',
+
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig: Config.InitialOptions = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
 };
 
-export default config;
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
