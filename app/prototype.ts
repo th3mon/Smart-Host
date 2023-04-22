5f487958e47975ef2396cefb8fa976108a6bee99
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-22 21:45:43 +0200

    Remove Prototype

--- app/prototype.ts
+++
@@ -1,8 +0,0 @@
-import { sortNumbersDescending } from './utils';
-
-export const getUsage = (guests: number[], rooms: number): number => {
-  return guests
-    .sort(sortNumbersDescending)
-    .slice(0, rooms)
-    .reduce((usage, guest) => usage + guest);
-};
