3fc4cd8cadb1a131f3ed1be91cefc0bdfe9dc334
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-04-28 23:20:35 +0200

    Implement UX/UI

--- app/components/room-occupancy-optimization/room-occupancy-optimization.tsx
+++ app/components/room-occupancy-optimization/room-occupancy-optimization.tsx
@@ -67,68 +67,76 @@ export const RoomOccupancyOptimization: React.FunctionComponent = () => {
   };
 
   return (
-    <form className="room-occupancy-optimization" onSubmit={calculateUsage}>
-      <div className="room-occupancy-optimization__premium-usage premium-usage">
-        <div className="premium-usage__title">
-          <h2>Premium Usage</h2>
+    <section className="room-occupancy-optimization bg-orange-400 rounded-xl py-8 px-10 text-gray-800">
+      <header className="room-occupancy-optimization__header text-center text-2xl mb-5">
+        <h2 className="room-occupancy-optimization__title">
+          Room occupancy optimization
+        </h2>
+      </header>
+      <form
+        onSubmit={calculateUsage}
+        className="grid grid-cols-2 grid-rows-2 gap-3 justify-center"
+      >
+        <div className="room-occupancy-optimization__premium-usage premium-usage">
+          <div className="premium-usage__rooms">
+            <label
+              htmlFor="premium-usage__rooms-input"
+              className="premium-usage__rooms-label"
+            >
+              Premium Rooms&nbsp;
+            </label>
+            <input
+              className="premium-usage__rooms-input text-black"
+              id="premium-usage__rooms-input"
+              type="number"
+              ref={premiumRoomsInputRef}
+            />
+          </div>
+
+          <h3 className="premium-usage__title text-right">
+            <span>Usage&nbsp;</span>
+            <span
+              className="premium-usage__value"
+              data-testid="premium-usage__value"
+            >
+              {roomsUsage.premium + ' EUR'}
+            </span>
+          </h3>
         </div>
 
-        <div className="premium-usage__rooms">
-          <label
-            htmlFor="premium-usage__rooms-input"
-            className="premium-usage__rooms-label"
-          >
-            Premium Rooms
-          </label>
-          <input
-            className="premium-usage__rooms-input text-black"
-            id="premium-usage__rooms-input"
-            type="number"
-            ref={premiumRoomsInputRef}
-          />
+        <div className="room-occupancy-optimization__economy-usage">
+          <div className="economy-usage__rooms">
+            <label
+              htmlFor="economy-usage__rooms-input"
+              className="economy-usage__rooms-label"
+            >
+              Economy Rooms&nbsp;
+            </label>
+            <input
+              className="economy-usage__rooms-input text-black"
+              id="economy-usage__rooms-input"
+              type="number"
+              ref={economyRoomsInputRef}
+            />
+          </div>
 
-          <p data-testid="premium-test">{rooms.premium}</p>
+          <div className="economy-usage__title text-right">
+            <span>Usage&nbsp;</span>
+            <span
+              className="economy-usage__value"
+              data-testid="economy-usage__value"
+            >
+              {roomsUsage.economy + ' EUR'}
+            </span>
+          </div>
         </div>
 
-        <div
-          className="premium-usage__value"
-          data-testid="premium-usage__value"
-        >
-          {roomsUsage.premium + ' EUR'}
+        <div className="col-span-2 flex justify-center">
+          <button className="room-occupancy-optimisation__button rounded-3xl bg-black text-orange-400 px-8 hover:bg-gray-800 active:translate-y-0.5">
+            calculate usage
+          </button>
         </div>
-      </div>
-
-      <div className="room-occupancy-optimization__economy-usage">
-        <div className="economy-usage__title">
-          <h2>Economy Usage</h2>
-        </div>
-
-        <div className="economy-usage__rooms">
-          <label
-            htmlFor="economy-usage__rooms-input"
-            className="economy-usage__rooms-label"
-          >
-            Economy Rooms
-          </label>
-          <input
-            className="economy-usage__rooms-input text-black"
-            id="economy-usage__rooms-input"
-            type="number"
-            ref={economyRoomsInputRef}
-          />
-
-          <p data-testid="economy-test">{rooms.economy}</p>
-        </div>
-
-        <div
-          className="economy-usage__value"
-          data-testid="economy-usage__value"
-        >
-          {roomsUsage.economy + ' EUR'}
-        </div>
-      </div>
-
-      <button>calculate usage</button>
-    </form>
+      </form>
+    </section>
   );
 };
