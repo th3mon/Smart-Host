2d4f3253b32189e430ccb9050df0dc582faf07b6
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-05-08 02:37:32 +0200

    Add GetPremiumAndUpgradedEconomyGuestsProps type

--- app/components/room-occupancy-optimization/get-premium-and-upgraded-economy-guests.ts
+++ app/components/room-occupancy-optimization/get-premium-and-upgraded-economy-guests.ts
@@ -2,13 +2,15 @@ import { pickHighestPayingGuests } from './pick-highest-paying-guests';
 import { EmptyRooms } from '@/app/components/room-occupancy-optimization/calculate-empty-rooms';
 import { Guests } from './pick-guests';
 
+export type GetPremiumAndUpgradedEconomyGuestsProps = {
+  guests: Guests;
+  emptyRooms: EmptyRooms;
+};
+
 export const getPremiumAndUpgradedEconomyGuests = ({
   guests,
   emptyRooms,
-}: {
-  guests: Guests;
-  emptyRooms: EmptyRooms;
-}): number[] =>
+}: GetPremiumAndUpgradedEconomyGuestsProps): number[] =>
   !emptyRooms.economy && emptyRooms.premium
     ? [
         ...guests.premium,
